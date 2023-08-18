a2878cb2ee587b7e66a48e389691b3ad0e8e05cb
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-08-18 13:46:28 +0000

    yyk

--- 3d_dich.py
+++ 3d_dich.py
@@ -15,7 +15,7 @@ def randrange(n, vmin, vmax):
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-n = 100
+n = 1000
 
 # For each set of style and range settings, plot n random points in the box
 # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
@@ -23,7 +23,7 @@ for m, zlow, zhigh in [(',', -50, -25), (',', -30, -5)]:
     xs = randrange(n, 23, 32)
     ys = randrange(n, 0, 100)
     zs = randrange(n, zlow, zhigh)
-    ax.scatter(xs, ys, zs, marker=m)
+    ax.scatter(xs, ys, zs, color='black')
 
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
